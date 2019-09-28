dfdf6d6d0fdcfef6205746ecc3f4bccd7daf8453
Andrew Goeden <andrewgoeden@gmail.com>
2019-09-27 21:49:21 -0400

    updated challenge 2

--- Challenge2.py
+++ Challenge2.py
@@ -9,28 +9,34 @@ M1 = [["B", "D", "G", "H", "L", "M", "P", "R", "S", "T"], ["A", "E", "H", "I", "
     , ["A", "D", "E", "K", "L", "N", "S", "T", "Y", ""]]
 count = 0
 
-write = open("possible_outcomes.txt","w+")
+
+
+# write = open("possible_outcomes.txt","w+")
 
 for x in numpy.array(numpy.meshgrid(*M1)).T.reshape(-1,len(M1)):
-    write.write(''.join(x) + "\n")
-    # if x[3] != "":
-    #     count += 1.5
-    # else:
-    #     count += 1
-    #
-    # if ''.join(x) == password:
-    #     print(count)
-    #     exit(0)
+    if x[3] != "":
+        count += 1.5
+    else:
+        count += 1
 
-# for new in itertools.product(*M1):
-#     print(new)
-#     # if new[3] != "":
-#     #     count =+ 1.5
-#     # else:
-#     #     count += 1
-#     #
-#     # if str(new) == password:
-#     #     print(count)
-#     #     exit(0)
-#
-# print(list(itertools.product(*M1)))
+    if ''.join(x) == password:
+        print(count)
+        exit(0)
+
+    #Create file of possible outcomes
+    # write.write(''.join(x) + "\n")
+
+#   inOrder
+for new in itertools.product(*M1):
+    # print(new)
+    if new[3] != "":
+        count += 1.5
+    else:
+        count += 1
+
+    if ''.join(new) == password:
+        print(count)
+        exit(0)
+
+# # print(list(itertools.product(*M1)))
+#   inOrder
