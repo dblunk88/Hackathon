b9650d692a21a9ba5c4c267e336ad0f032046365
Andrew Goeden <andrewgoeden@gmail.com>
2019-09-27 23:19:03 -0400

    Finished Challenge 2 without time average

--- Challenge2.py
+++ Challenge2.py
@@ -1,8 +1,5 @@
 import PassCrack
 
-read3 = open("3.txt", "r")
-read4 = open("4.txt", "r")
-
 
 password = input("ENTER ZEH PASSWORD: ")
 print("Muhchines arr fihring, Mahster: " + password)
@@ -10,9 +7,8 @@ print("Muhchines arr fihring, Mahster: " + password)
 M1 = [["B", "D", "G", "H", "L", "M", "P", "R", "S", "T"], ["A", "E", "H", "I", "L", "N", "O", "R", "U", "Y"]
     , ["A", "C", "E", "L", "N", "O", "R", "S", "T", "U"]
     , ["A", "D", "E", "K", "L", "N", "S", "T", "Y", ""]]
-count = 0
+# count = 0
 
-write = open("possible_outcomes.txt","w+")
+# write = open("possible_outcomes.txt","w+")
 
 print(PassCrack.randomSearch(M1, "BURY"))
-# PassCrackClass.linearSearch()
--- PassCrack.py
+++ PassCrack.py
@@ -6,28 +6,41 @@ def randomSearch(arr, key):
     count = 0
 
     for x in numpy.array(numpy.meshgrid(*arr)).T.reshape(-1, len(arr)):
-        if x[3] != "":
-            count += 1.5
-        else:
-            count += 1
+        combination = ''.join(x)
+        read = open("challenge2.txt", "r")
+        for word in read.read().split("\n"):
+            if combination == word:
+                if combination == key:
+                    read.close()
+                    return count
+                else:
+                    if x[3] != "":
+                        count += 1.5
+                        read.close()
+                        continue
 
-        if ''.join(x) == key:
-            return count
+                    else:
+                        count += 1
+                        read.close()
+                        continue
+            else:
+                continue
 
         # Create file of possible outcomes
         # write.write(''.join(x) + "\n")
 
 
-def linearSearch(arr, key):
-    count = 0
-
-    for x in itertools.product(*arr):
-        if x[3] != "":
-            count += 1.5
-        else:
-            count += 1
-
-        if ''.join(x) == key:
-            return count
-
-    # # print(list(itertools.product(*M1)))
+# not linear
+# def linearSearch(arr, key):
+#     count = 0
+#
+#     for x in itertools.product(*arr):
+#         if x[3] != "":
+#             count += 1.5
+#         else:
+#             count += 1
+#
+#         if ''.join(x) == key:
+#             return count
+#
+#     # # print(list(itertools.product(*M1)))
