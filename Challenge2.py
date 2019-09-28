50ff533bbc13d132a612ffe0419f429500d81109
Andrew Goeden <andrewgoeden@gmail.com>
2019-09-27 22:01:57 -0400

    new changes to challenge 2 using the new class

--- Challenge2.py
+++ Challenge2.py
@@ -1,5 +1,6 @@
 import itertools
 import numpy
+import PassCrack
 
 password = input("ENTER ZEH PASSWORD: ")
 print("Muhchines arr fihring, Mahster: " + password)
@@ -9,34 +10,5 @@ M1 = [["B", "D", "G", "H", "L", "M", "P", "R", "S", "T"], ["A", "E", "H", "I", "
     , ["A", "D", "E", "K", "L", "N", "S", "T", "Y", ""]]
 count = 0
 
-
-
-# write = open("possible_outcomes.txt","w+")
-
-for x in numpy.array(numpy.meshgrid(*M1)).T.reshape(-1,len(M1)):
-    if x[3] != "":
-        count += 1.5
-    else:
-        count += 1
-
-    if ''.join(x) == password:
-        print(count)
-        exit(0)
-
-    #Create file of possible outcomes
-    # write.write(''.join(x) + "\n")
-
-#   inOrder
-for new in itertools.product(*M1):
-    # print(new)
-    if new[3] != "":
-        count += 1.5
-    else:
-        count += 1
-
-    if ''.join(new) == password:
-        print(count)
-        exit(0)
-
-# # print(list(itertools.product(*M1)))
-#   inOrder
+print(PassCrack.randomSearch(M1, "BURY"))
+# PassCrackClass.linearSearch()
